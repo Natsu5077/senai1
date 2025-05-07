8846a198a74cf66bfe03c699308352c26f4a04e0
= <rafaelvieirapdm@gmail.com>
2025-05-07 09:45:49 -0300

    Dados

--- src/App.java
+++ src/App.java
@@ -2,52 +2,50 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git");
-    // cara ou coroa - probabilidade
-    // 50% cara 50% coroa
+        System.out.println("Bem-Vindo ao lançamento de dados. Quer apostar hoje?");
+   //probabilidade
     //espaço amostral
     // criar uma variavel que recebe a interface do jogo
     //Random
     //Switch
     // for, do while e while 
     
-    String [] espaçoamostral= {"CARA","COROA"};
+    int [] espaçoamostral= new int [6];
         //objeto random
         //sorteio
         Random random = new Random();
         // contador para verificação
-        int contCara =0;
-        int contCoroa =0;
-        String menssagem= "Espaço amostral: Cara ou Coroa: "+
-                        "A probabilidade de cara: 50%\n "+
-                        "probabilidade de coroa: 50%\n " +
-                        "deseja lancar a moeda? ";
+        int totaljogadas= 0;
+        String menssagem= "Espaço amostral: 1 a 6 "+
+                        "Cada número tem probabilidade inicial de 16,67%\n"+
+                        "deseja lançar o dado? E que tal uma aposta? ";
         //loop de lançamento
         while (true) {
         //interação com  o usuário
-        int opcao = JOptionPane.showConfirmDialog(null, menssagem, "Lançando moedas", JOptionPane.YES_NO_OPTION);
+        int opcao = JOptionPane.showConfirmDialog(null, menssagem, "Lançandor de dado", JOptionPane.YES_NO_OPTION);
         
         if (opcao != JOptionPane.YES_OPTION) {
 
             //estatisticas
-            String estatistica = "jogo finalizado \n "+
-                                "total de lançamentos: "+ (contCara+contCoroa)+"\n "+
-                                "caras: " +contCara +"\n "+
-                                "coroas: " +contCoroa +"\n";
-                                JOptionPane.showMessageDialog(null,  estatistica, "Finalizando jogo", JOptionPane.INFORMATION_MESSAGE);
-                            
-
+            String estatistica = "jogo finalizado \n  "+
+                                "total de lançamentos:  "+ totaljogadas+"\n ";
+                            for (int  i= 0; i <6; i ++){
+                                estatistica += "Número " + (i+1) +": "+ espaçoamostral [i]+ " vezes \n";
+                             } JOptionPane.showMessageDialog(null,  estatistica, "Finalizando jogo", JOptionPane.INFORMATION_MESSAGE);
             break;
         }        
-        int resultado =random.nextInt(2); //0-cara 1- coroa
-        if (resultado==0) {
-            contCara++;
-        } else{
-            contCoroa++;
-        }
-            String saida= "resultado de lançamento: " + espaçoamostral[resultado]+
-                            "\n indice gerado: "+ resultado +")";
-                            JOptionPane.showMessageDialog(null,  saida, "Resultado",JOptionPane.INFORMATION_MESSAGE);
+        // finalização 
+        int resultado =random.nextInt(6) +1; 
+        espaçoamostral [ resultado - 1]++;
+        totaljogadas ++;
+        double prob1 = 1.0 / 6.0; 
+        double prob2 = (double) espaçoamostral[resultado - 1] / totaljogadas; 
+        double prob1e2 = prob1; 
+        double bayes = (prob2 == 0) ? 0 : (prob1e2 * prob1) / prob2;        
+        String saida = "Resultado do lançamento: " + resultado + "\n" +
+        "Probabilidade teorema de Bayes de sair " + resultado + ": " + String.format("%.4f", bayes);
+        JOptionPane.showMessageDialog(null, saida, "Resultado", JOptionPane.INFORMATION_MESSAGE);
+                          
     }
     
 }
