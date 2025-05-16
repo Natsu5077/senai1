f2ae48da5ff326c2a5cf0b440f71087fa4cee549
= <rafaelvieirapdm@gmail.com>
2025-05-16 09:31:44 -0300

    duvida 13 orientada a classe

---
+++ src/Duvida13.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+public class Duvida13{
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, 
+    calculodaarea(100 , 200));
+  }
+  public static double calculodaarea (double i, double c) {
+    return i*c;
+  }
+}
