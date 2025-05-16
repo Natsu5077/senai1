bcdb741ce1a9bb25cbeb73c17d0c74aa93575e56
= <rafaelvieirapdm@gmail.com>
2025-05-16 11:47:03 -0300

    terminar de refazer a 06.

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import javax.print.attribute.standard.JobHoldUntil;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+ class Cliente
+ {
+    public String nome, telefone, email, cep, lote, complemento;
+    public Cliente (String nome, String telefone, String email, String cep, String lote, String complemento){
+    this.nome= nome;
+    this.telefone = telefone;
+    this.email= email;
+    this.cep= cep;
+    this.lote = lote;
+    this.complemento = complemento;
+}
+public void exibir(){
+    
+}
+
+}
+
+public class App{
+ 
+}
